63bd186a830f49540d0cfb8cd32036a6e63dd4ca
flibbit31 <flibbit31@yahoo.com>
2022-02-05 12:59:01 -0700

    Added greeting to Arturo.

--- src/CristecheBellone/Main.java
+++ src/CristecheBellone/Main.java
@@ -3,6 +3,7 @@ package CristecheBellone;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Arturo");
+	    System.out.println("Hello, my name is Arturo");
+        System.out.println("Hello Arturo, my name is Benjamin.");
     }
 }
